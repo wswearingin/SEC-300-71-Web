63ca3d971b30626cc11f3dff047f7e89c7fa38c5
wswearingin <william@swearing.in>
2024-10-15 14:29:36 -0400

    Added input validation, changed website to registration form.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace SSD_web.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(14, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 12 characters.")]
         public string firstName {  get; set; }
-        public string preference {  get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string email {  get; set; }
 
         private readonly ILogger<IndexModel> _logger;
 
@@ -15,10 +23,15 @@ namespace SSD_web.Pages
             _logger = logger;
         }
 
-        public void OnGet(string firstName, string preference)
+        //public void OnGet(string firstName, string email)
+        //{
+        //    this.firstName = firstName;
+        //    this.email = email;
+        //}
+
+        public IActionResult OnPost()
         {
-            this.firstName = firstName;
-            this.preference = preference;
+            return Page();
         }
     }
 }
